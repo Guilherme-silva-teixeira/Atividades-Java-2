16141454eb421dbaa9cfaa2d943530913e08838d
Guilherme <132455461+Guilherme-silva-teixeira@users.noreply.github.com>
2025-01-29 21:08:20 -0300

    Create Funcionario.java

---
+++ Questão-03/Funcionario.java
@@ -0,0 +1,29 @@
+class Funcionario extends Pessoa implements Trabalhador
+{
+    protected double salario;
+
+    public Funcionario(String nome, int idade, double salario)
+    {
+        super(nome, idade);
+        this.salario = salario;
+    }
+
+    @Override
+    public void exibirInfo()
+    {
+        System.out.println("Funcionário: " + nome + ", Idade: " + idade + ", Salário: R$" + salario);
+    }
+
+    @Override
+    public void calcularPagamento()
+    {
+        System.out.println(nome + " recebe um salário fixo de R$" + salario);
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Funcionário: " + nome + " | Idade: " + idade + " | Salário: R$" + salario;
+    }
+    
+}
