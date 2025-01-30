cd7715ae6d47850924fe8d518caf6f3de91ecf2e
Guilherme <132455461+Guilherme-silva-teixeira@users.noreply.github.com>
2025-01-29 21:07:30 -0300

    Create Pessoa.java

---
+++ Questão-03/Pessoa.java
@@ -0,0 +1,19 @@
+
+interface Trabalhador
+{
+    void calcularPagamento();
+}
+
+abstract class Pessoa
+{
+    protected String nome;
+    protected int idade;
+
+    public Pessoa(String nome, int idade)
+    {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public abstract void exibirInfo();
+}
